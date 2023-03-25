5f4f73ad6d968a5f5a2d5549bf89561c8e01273f
Slickhit <91316272+Slickhit@users.noreply.github.com>
2023-03-25 10:35:42 -0400

    Update capture_command_output_and_notify.sh

--- capture_command_output_and_notify.sh
+++ capture_command_output_and_notify.sh
@@ -5,8 +5,15 @@
 # Define the command to run and capture the output
 command="ls -la /path/to/directory"
 
-# Capture the output of the command in a variable
-output=$($command)
+# Run the command, capture the output, and send the email notification every 5 minutes
+while true; do
+  # Capture the output of the command in a variable
+  output=$($command)
 
-# Send an email notification with the captured output
-echo "$output" | mail -s "Command Output Notification" guzman77@live.com
+  # Send an email notification with the captured output
+  echo "$output" | mail -s "Command Output Notification" guzman77@live.com
+
+  # Wait for 5 minutes (300 seconds) before running the command again
+  sleep 300
+done
+
